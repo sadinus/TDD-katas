4aea1d31f2568c01bda7f34c370d518656a13ba5
Paweł Pindelski <pindelski.pawel@gmail.com>
2021-07-26 13:07:21 +0200

    Done FizzBuzz kata

---
+++ ConsoleApp/FizzBuzz/FizzBuzzConsole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.FizzBuzz
+{
+    public class FizzBuzzConsole
+    {
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
+        private const string FizzBuzz = "FizzBuzz";
+
+        public string WriteText(int number)
+        {
+            if (number < 1 || number > 100) throw new ArgumentException("provide a number: from 1 to 100");
+
+            if (number % 15 == 0) return FizzBuzz;
+            else if (number % 5 == 0) return Buzz;
+            else if (number % 3 == 0) return Fizz;
+            return number.ToString();
+        }
+    }
+}
---
+++ Tests/FizzBuzzTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp.FizzBuzz;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        FizzBuzzConsole console;
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
+        private const string FizzBuzz = "FizzBuzz";
+
+        [SetUp]
+        public void Setup()
+        {
+            console = new FizzBuzzConsole();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(101)]
+        public void WriteText_ShouldReturnInvalidNumberWhenOutOfRange(int number)
+        {
+            Assert.That(() => console.WriteText(number), Throws.TypeOf<ArgumentException>()
+                              .With
+                              .Property("Message")
+                              .EqualTo("provide a number: from 1 to 100"));
+        }
+
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, Fizz)]
+        [TestCase(4, "4")]
+        [TestCase(5, Buzz)]
+        [TestCase(9, Fizz)]
+        [TestCase(10, Buzz)]
+        [TestCase(15, FizzBuzz)]
+        public void Output_ShouldReturnCorrectValues(int number, string expected)
+        {
+            var actual = console.WriteText(number);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
