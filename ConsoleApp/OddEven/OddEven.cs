715d52da8676e6574a60a5fdc7dbdacf48ade5c0
Paweł Pindelski <pindelski.pawel@gmail.com>
2021-07-27 22:45:03 +0200

    Done OddEven kata

---
+++ ConsoleApp/OddEven/OddEven.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class OddEven
+    {
+        private const string Even = "Even";
+        private const string Odd = "Odd";
+
+        public string CheckNumber(int number, int bottomLimit = 1, int topLimit = 100)
+        {
+            if (IsOutOfRange(number, bottomLimit, topLimit)) throw new ArgumentException($"You can only provide number from {bottomLimit} to {topLimit}.");
+            if (isPrime(number)) { return number.ToString(); }
+            if (IsEven(number)) return Even;
+            if (IsOdd(number)) return Odd;
+
+            throw new ArgumentException();
+        }
+
+        private bool isPrime(int number)
+        {
+            if (number == 1 || number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        private bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        private bool IsOutOfRange(int number, int bottomLimit, int topLimit)
+        {
+            return number > topLimit || number < bottomLimit;
+        }
+    }
+}
---
+++ Tests/OddEvenTests.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleApp;
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    class OddEvenTests
+    {
+        private const string Even = "Even";
+        private const string Odd = "Odd";
+        private OddEven oddEven;
+
+        [SetUp]
+        public void Setup()
+        {
+            oddEven = new OddEven();
+        }
+
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "3")]
+        [TestCase(4, Even)]
+        [TestCase(5, "5")]
+        [TestCase(6, Even)]
+        [TestCase(9, Odd)]
+        [TestCase(15, Odd)]
+        [TestCase(100, Even)]
+        public void OddEven_ShouldPrintNumbers(int number, string expected)
+        {
+            var actual = oddEven.CheckNumber(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase(101, 0, 100)]
+        [TestCase(-1, 0, 100)]
+        [TestCase(50, -20, 20)]
+        [TestCase(7, 4, 6)]
+        public void OddEven_ShouldNotPrintNumbersOutOfRange(int number, int bottomLimit, int topLimit)
+        {
+            Assert.That(() => oddEven.CheckNumber(number, bottomLimit, topLimit), Throws.TypeOf<ArgumentException>()
+                .With
+                .Property("Message")
+                .EqualTo($"You can only provide number from {bottomLimit} to {topLimit}."));
+        }
+
+        [Test]
+        [TestCase(150, -20, 200, Even)]
+        [TestCase(5, 4, 6, "5")]
+        public void OddEven_ShouldPrintNumbersInRange(int number, int bottomLimit, int topLimit, string expected)
+        {
+            var actual = oddEven.CheckNumber(number, bottomLimit, topLimit);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
