a3e64277de327ef5bb17692076c89de8eb4e1b99
Paweł Pindelski <pindelski.pawel@gmail.com>
2021-07-28 19:54:46 +0200

    Done Prime Factors

---
+++ ConsoleApp/PrimeFactors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    public class PrimeFactors
+    {
+        public List<int> Generate(int number)
+        {
+            var primeFactors = new List<int>();
+
+            if (number == 1) return primeFactors;
+            for (int divider = 2; divider <= number; divider++)
+            {
+                while (number % divider == 0)
+                {
+                    primeFactors.Add(divider);
+                    number = number / divider;
+                }
+            }
+
+            primeFactors.Sort();
+
+            return primeFactors;
+        }
+    }
+}
---
+++ Tests/PrimeFactorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    class PrimeFactorTests
+    {
+        PrimeFactors primeFactors;
+
+        [SetUp]
+        public void Setup()
+        {
+            primeFactors = new PrimeFactors();
+        }
+
+        private static readonly object[] DATA =
+        {
+            new object[] {1, new List<int> {}},
+            new object[] {2, new List<int> {2}},
+            new object[] {3, new List<int> {3}},
+            new object[] {4, new List<int> {2, 2}},
+            new object[] {6, new List<int> {2, 3}},
+            new object[] {8, new List<int> {2, 2, 2}},
+            new object[] {24, new List<int> { 2, 2, 3, 2} }
+        };
+
+        [Test]
+        [TestCaseSource(nameof(DATA))]
+        public void Generate_ShouldReturnPrimeNumbers(int number, List<int> expected)
+        {
+            var actual = primeFactors.Generate(number);
+
+            expected.Sort();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
