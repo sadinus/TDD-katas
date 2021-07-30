c271971e3f76d184210bf92af4865bb13a5db364
Paweł Pindelski <pindelski.pawel@gmail.com>
2021-07-30 11:36:18 +0200

    Done Game of Life kata

---
+++ ConsoleApp/GameOfLife.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class GameOfLife
+    {
+        private const char ALIVE = '*';
+        private const char DEAD =  '.';
+
+        public char[,] NextGeneration(char[,] grid)
+        {
+            var gridCopy = grid.Clone() as char[,];
+            for (int XPosition = 0; XPosition < grid.GetLength(0); XPosition++)
+            {
+                for (int YPosition = 0; YPosition < grid.GetLength(1); YPosition++)
+                {
+                    var currentCell = gridCopy[XPosition, YPosition];
+                    var numberOfNeighbours = GetNumberOfAliveNeighboars(gridCopy, XPosition, YPosition);
+
+                    if (IsAliveCell(currentCell) && numberOfNeighbours < 2)
+                    {
+                        grid[XPosition, YPosition] = DEAD;
+                    }
+
+                    if (IsAliveCell(currentCell) && numberOfNeighbours > 3)
+                    {
+                        grid[XPosition, YPosition] = DEAD;
+                    }
+
+                    if (IsDeadCell(currentCell) && numberOfNeighbours == 3)
+                    {
+                        grid[XPosition, YPosition] = ALIVE;
+                    }
+                }
+            }
+
+            return grid;
+        }
+
+        private int GetNumberOfAliveNeighboars(char[,] grid, int XPosition, int YPosition)
+        {
+            var result = 0;
+            int xStart;
+            int xEnd;
+            int yStart;
+            int yEnd;
+
+            xStart = XPosition > 0 ? XPosition - 1 : XPosition;
+            xEnd = XPosition < grid.GetUpperBound(0) ? XPosition + 1 : XPosition;
+            yStart = YPosition > 0 ? YPosition - 1 : YPosition;
+            yEnd = YPosition < grid.GetUpperBound(1) ? YPosition + 1 : YPosition;
+
+            for (int currentX = xStart; currentX <= xEnd; currentX++)
+            {
+                for (int currentY = yStart; currentY <= yEnd; currentY++)
+                {
+                    var isNeighbour = IsNeighbour(XPosition, YPosition, currentX, currentY);
+                    if (isNeighbour && IsAliveCell(grid[currentX, currentY]))
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsNeighbour(int XPosition, int YPosition, int currentX, int currentY) 
+        {
+            return !(XPosition == currentX && YPosition == currentY);
+        }
+
+        private bool IsAliveCell(char cell)
+        {
+            return cell == ALIVE;
+        }
+
+        private bool IsDeadCell(char cell)
+        {
+            return cell == DEAD;
+        }
+    }
+}
---
+++ Tests/GameOfLifeTests.cs
@@ -0,0 +1,94 @@
+﻿using ConsoleApp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    class GameOfLifeTests
+    {
+        GameOfLife gameOfLife;
+
+        [SetUp]
+        public void Setup()
+        {
+            gameOfLife = new GameOfLife();
+        }
+
+        [Test]
+        public void NextGeneration_ShouldKillCellsWithLessThanTwoNeighbours()
+        {
+            var input = new char[4, 4] { { '.', '.', '.', '.' },
+                                         { '.', '*', '*', '.' },
+                                         { '.', '.', '.', '.' },
+                                         { '.', '.', '.', '.' } };
+
+            var expected = new char[4, 4] { { '.', '.', '.', '.' },
+                                            { '.', '.', '.', '.' },
+                                            { '.', '.', '.', '.' },
+                                            { '.', '.', '.', '.' } };
+
+            var actual = gameOfLife.NextGeneration(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NextGeneration_ShouldKillCellsWithMoreThanThreeNeighbours()
+        {
+            var input = new char[3, 4] { { '.', '*', '*', '*' },
+                                         { '.', '.', '*', '.' },
+                                         { '.', '*', '*', '*' } };
+
+            var expected = new char[3, 4] { { '.', '*', '*', '*' },
+                                            { '.', '.', '.', '.' },
+                                            { '.', '*', '*', '*' } };
+
+            var actual = gameOfLife.NextGeneration(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NextGeneration_ShouldIdleAliveCellsWithTwoOrThreeNeighbours()
+        {
+            var input = new char[4, 4] { { '*', '*', '*', '*' },
+                                         { '*', '.', '.', '*' },
+                                         { '*', '.', '.', '*' },
+                                         { '*', '*', '*', '*' } };
+
+            var expected = new char[4, 4] { { '*', '*', '*', '*' },
+                                            { '*', '.', '.', '*' },
+                                            { '*', '.', '.', '*' },
+                                            { '*', '*', '*', '*' } };
+
+            var actual = gameOfLife.NextGeneration(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NextGeneration_ShouldBornDeadCellsWithThreeNeighbours()
+        {
+            var input = new char[5, 4] { { '*', '*', '*', '*' },
+                                         { '*', '.', '.', '*' },
+                                         { '*', '.', '.', '*' },
+                                         { '*', '.', '.', '*' },
+                                         { '*', '*', '*', '*' }};
+
+            var expected = new char[5, 4] { { '*', '*', '*', '*' },
+                                            { '*', '.', '.', '*' },
+                                            { '*', '*', '*', '*' },
+                                            { '*', '.', '.', '*' },
+                                            { '*', '*', '*', '*' }};
+
+            var actual = gameOfLife.NextGeneration(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
