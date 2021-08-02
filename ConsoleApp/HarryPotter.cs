e45087a1765a7be093a3b5b662b6622231c9750b
Paweł Pindelski <pindelski.pawel@gmail.com>
2021-08-02 13:15:41 +0200

    HarryPotter checkpoint

---
+++ ConsoleApp/HarryPotter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class HarryPotter
+    {
+        private const decimal BASIC_PRICE = 8;
+        private const string CURRENCY = "EUR";
+
+        public string CalculatePrice(List<Volume> books)
+        {
+            var basket = (from b in books
+                          group b by b into g
+                          select new BooksVolume { Volume = g.Key, Total = g.Count() }).OrderBy(x => x.Total).ToList();
+
+            var totalPrice = CalculateSetOfBooksPrice(basket);
+            var result = FormatPrice(totalPrice);
+
+            return result;
+        }
+
+        private decimal CalculateSetOfBooksPrice(List<BooksVolume> basket)
+        {
+            decimal totalPrice = 0M;
+
+            while (basket.Count > 0)
+            {
+                var groupNumber = basket.FirstOrDefault().Total;
+
+                var booksInSet = basket.Count();
+
+                var discount = CalculateDiscount(booksInSet);
+
+                totalPrice += groupNumber * discount * booksInSet * BASIC_PRICE;
+
+                basket.RemoveAll(x => x.Total == groupNumber);
+                basket.ForEach(x => x.Total -= groupNumber);
+            }
+
+            return totalPrice;
+        }
+
+        private decimal CalculateDiscount(int booksTypeNumber)
+        {
+            switch (booksTypeNumber)
+            {
+                case 2:
+                    return 0.95M;
+                case 3:
+                    return 0.9M;
+                case 4:
+                    return 0.8M;
+                case 5:
+                    return 0.75M;
+                default:
+                    return 1;
+            }
+        }
+
+        private string FormatPrice(decimal price)
+        {
+            var priceWithTwoDecimalPlaces = string.Format("{0:0.00}", price);
+            return $"{priceWithTwoDecimalPlaces} {CURRENCY}";
+        }
+    }
+
+    public class BooksVolume
+    {
+        public Volume Volume { get; set; }
+        public int Total { get; set; }
+    }
+
+    public enum Volume
+    {
+        First,
+        Second,
+        Third,
+        Fourth,
+        Fifth
+    }
+}
---
+++ Tests/HarryPotterTests.cs
@@ -0,0 +1,85 @@
+﻿using ConsoleApp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class HarryPotterTests
+    {
+        [Test]
+        public void CalculatePrice_TwoSameBooksShouldNotApplyDiscount()
+        {
+            var books = new List<Volume>();
+            books.Add(Volume.First);
+            books.Add(Volume.First);
+
+            var expected = "16,00 EUR";
+
+            var harryPotter = new HarryPotter();
+
+            var actual = harryPotter.CalculatePrice(books);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CalculatePrice_SingleSetShouldApplyDiscount()
+        {
+            var books = new List<Volume>();
+            books.Add(Volume.First);
+            books.Add(Volume.Second);
+
+            var expected = "15,20 EUR";
+
+            var harryPotter = new HarryPotter();
+
+            var actual = harryPotter.CalculatePrice(books);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CalculatePrice_TwoDifferentBookSetsShouldApplyDiscount()
+        {
+            var books = new List<Volume>();
+            books.Add(Volume.First);
+            books.Add(Volume.Second);
+            books.Add(Volume.Third);
+            books.Add(Volume.First);
+            books.Add(Volume.Second);
+
+            var expected = "36,80 EUR";
+
+            var harryPotter = new HarryPotter();
+
+            var actual = harryPotter.CalculatePrice(books);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CalculatePrice_ShouldApplyDiscountOnlyForSets()
+        {
+            var books = new List<Volume>();
+            books.Add(Volume.First);
+            books.Add(Volume.First);
+            books.Add(Volume.Second);
+            books.Add(Volume.Second);
+            books.Add(Volume.Third);
+            books.Add(Volume.Third);
+            books.Add(Volume.Fourth);
+
+            var expected = "51,20 EUR";
+
+            var harryPotter = new HarryPotter();
+
+            var actual = harryPotter.CalculatePrice(books);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
